6b6b58e355bfa630c12bbb9d30ec7b95433cff55
Kari Rice <kari.rice@gmail.com>
2023-03-04 17:45:14 -0700

    Alphabetize and print results

--- ratings.py
+++ ratings.py
@@ -23,4 +23,17 @@ def create_rest_ratings_dict(read_file):
         ratings_dict[line_split[0]] = line_split[1] 
     
     return ratings_dict 
-        
+
+def alphabetize_rest_ratings(ratings_dict):
+    '''Alphabetize the dictionary from function create_rest_ratings_dict'''
+
+    restaurants_sorted = sorted(ratings_dict.keys())
+
+    
+    for restaurant in restaurants_sorted:
+        print(f"{restaurant} is rated at {ratings_dict[restaurant]}")
+
+
+ratings_dictionary = create_rest_ratings_dict(read_file("scores.txt"))
+
+alphabetize_rest_ratings(ratings_dictionary)
