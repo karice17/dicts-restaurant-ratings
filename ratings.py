33f2ede75ecfd93c6aae257f564c99aa921bfb02
Kari Rice <kari.rice@gmail.com>
2023-03-04 17:25:17 -0700

    Create ratings dictionary

--- ratings.py
+++ ratings.py
@@ -1,4 +1,26 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+ #define funtion
+ 
+def read_file(filename):
+    '''read text file and return list of lines'''
+
+    text = open(filename)
+
+    #read lines
+    #sample data "Donut You Want Me Baby:1"
+    return text.readlines()  # ["Penne For Your Thoughts:4", "Ramen On Empty:3", ...]
+    
+
+def create_rest_ratings_dict(read_file):
+    ''' take list from read_file function and create dictonary of results'''
+
+    ratings_dict = {}  
+    # {"Penne For Your Thoughts": "4"}
+    for line in read_file:
+        line_split = line.split(":") 
+        ratings_dict[line_split[0]] = line_split[1] 
+    
+    return ratings_dict 
+        
